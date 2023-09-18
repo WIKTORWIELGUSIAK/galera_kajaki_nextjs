e29dff2906aceb38065bc6c954a86e9059e33104
WiktorWielgusiak <witia16@gmail.com>
2023-09-19 01:34:41 +0200

    Chore: Delete no-store in fetchRoads

--- app/api/fetchRoads.GET.ts
+++ app/api/fetchRoads.GET.ts
@@ -3,9 +3,7 @@ import { API, endpoints } from "@/api/endpoints";
 
 export default async function fetchRoads(): Promise<Road[]> {
   try {
-    const response = await fetch(`${API}/${endpoints.roads.getRoads}`, {
-      cache: "no-store",
-    });
+    const response = await fetch(`${API}/${endpoints.roads.getRoads}`);
     if (!response.ok) {
       const errorMessage = `Failed to fetch roads data: ${response.status} ${response.statusText}`;
       throw new Error(errorMessage);
